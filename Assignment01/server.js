ffc0fabfaa3caa99cccb1cef2b64e4a9579f9ae0
surajgoture216-maker <surajgoture216@gmail.com>
2026-03-21 10:27:06 +0530

    added assignments

---
+++ Assignment01/server.js
@@ -0,0 +1,46 @@
+const express=require("express")
+const app=express()
+const bodyPaser=require("body-parser")
+const bcrptjs=require('bcryptjs')
+const cors = require('cors')
+app.use(cors({ origin: 'http://localhost:4200' }))
+app.use(bodyPaser.json())
+app.get("/",(req,res)=>{
+  res.send("server is running successfully")
+})
+const users=[]
+app.get('/users',(req,res)=>{
+    res.json(users)
+})
+app.post('/users',(req,res)=>
+{
+  const {name,gmail,password}=req.body
+  const hashedPassword=bcrptjs.hashSync(password,10)
+  const user = {
+    id: users.length + 1,
+    name,
+    gmail,
+    password:hashedPassword
+  }
+  users.push(user)
+  res.json(users)
+})
+app.post('/api/login',(req,res)=>
+{
+  const{gmail,password}=req.body
+  const user=users.find((user)=>user.gmail===gmail)
+  if(!user)
+  {
+    return res.status(401).json({message:'invalid credential'})
+  }
+  const isValidPassword=bcrptjs.compareSync(password,user.password)
+  if(!isValidPassword)
+  {
+    return res.status(401).json({message:'invalid credential'})
+  }
+  res.json({message :'login successfull',user})
+
+})
+app.listen(3000,()=>{
+    console.log("server started on port 3000")
+})
